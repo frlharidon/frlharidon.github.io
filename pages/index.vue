863e46d0640ad918c0d2622b1d478c63bd6cad3b
Thibaut Davoult <thibaut.davoult@gmail.com>
2019-02-03 00:24:21 +0100

    fixing terser version

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,22 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
       <h1 class="title">
-        portfolio
+        Thibaut Davoult
       </h1>
       <h2 class="subtitle">
-        Nuxt.js project
+        Peronal Website
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+      <h3>
+        2017 - Now: Growth Engineer at <a href="https://livestorm.co/">Livestorm</a>
+      </h3>
+      <p>Test: <span v-show="seen">now you see me</span></p>
+
+      <ul id="v-for-object">
+        <li v-for="job in jobs" :key="job.id">
+            {{ job }}
+        </li>
+      </ul>
     </div>
   </section>
 </template>
@@ -28,6 +27,15 @@ import AppLogo from '~/components/AppLogo.vue'
 export default {
   components: {
     AppLogo
+  },
+  data() {
+    return {
+      seen: false,
+      jobs: {
+        lastJob: 'Growth Engineer at Livestorm',
+        prevJob: 'Growth & Marketing at Wisembly',
+      }
+    }
   }
 }
 </script>
@@ -36,23 +44,20 @@ export default {
 .container {
   min-height: 100vh;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
 }
 
 .title {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
-  font-size: 100px;
+  font-size: 50px;
   color: #35495e;
   letter-spacing: 1px;
 }
 
 .subtitle {
   font-weight: 300;
-  font-size: 42px;
+  font-size: 30px;
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
@@ -62,4 +67,3 @@ export default {
   padding-top: 15px;
 }
 </style>
-
